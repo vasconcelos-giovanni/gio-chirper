e7af005a121436785864fba38936e642dab0be48
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2023-07-26 11:33:57 -0300

    style(welcome): make website snipped responsive with modbile screens

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -64,6 +64,7 @@ defineProps({
             <img
                 src="../../../public/img/welcome-page/pc.png"
                 alt="Web site snippet"
+                class="w-full h-auto sm:max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg"
             />
             <div class="text-5xl font-thin text-gray-600">
                 Welcome to GioChirps!
@@ -80,11 +81,6 @@ defineProps({
     .dark\:bg-dots-lighter {
         background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
     }
-}
-
-.welcome-message {
-    font-size: 4rem;
-    font-weight: 300;
 }
 
 .logo {
